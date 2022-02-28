baac3d9b567ddbbd2746f7c1c299ca921b1f89b6
y4nstatic <97829565+y4nstatic@users.noreply.github.com>
2022-02-28 18:56:53 +0530

    Add files via upload

--- assets/js/banner_check.js
+++ assets/js/banner_check.js
@@ -1,5 +1,21 @@
+var isRunning = 1;
 var check_banner = setInterval(function()
 {
+    try
+    {
+        var socialBanners = document.getElementsByClassName("disclaimer");
+        if (socialBanners != null)
+        {
+            for (var bannerNo = 0; bannerNo < socialBanners.length; bannerNo++)
+            {
+                socialBanners[bannerNo].remove();
+            }
+            isRunning = 0;
+            stopInterval(check_banner);
+        }
+    }
+    catch (e)
+    {}
     body_div_elements = document.getElementsByTagName("div");
     for (element_number = 0; element_number < body_div_elements.length; element_number++)
     {
@@ -8,6 +24,7 @@ var check_banner = setInterval(function()
             if (body_div_elements[element_number].children[0].children[0].src == "https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png")
             {
                 body_div_elements[element_number].remove();
+                isRunning = 0;
                 stopInterval(check_banner);
                 break;
             }
